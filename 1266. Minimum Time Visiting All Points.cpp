114c8d46c2189fe8184233ce01240e375ee53240
Sam <57521056+sam7834@users.noreply.github.com>
2020-08-11 11:31:17 +0530

    Create 1266. Minimum Time Visiting All Points.cpp
    https://leetcode.com/problems/minimum-time-visiting-all-points/

---
+++ 1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int c=0;
+        for(int i =1; i< points.size(); i++){
+            int x = points[i][0] - points[i-1][0];
+            int y = points[i][1] - points[i-1][1];
+            c +=  max(abs(x), abs(y));
+        }
+        return c;
+    }
+};
