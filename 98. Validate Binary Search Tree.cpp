201f3888491abb1e84945d1a4f007cd09ecbc9bf
Sam <57521056+sam7834@users.noreply.github.com>
2020-08-31 15:07:57 +0530

    Create 98. Validate Binary Search Tree.cpp
    https://leetcode.com/problems/validate-binary-search-tree/
    https://leetcode.com/problems/validate-binary-search-tree/discuss/344672/C%2B%2B-clear-recursive-solution-without-using-longlongdouble

---
+++ 98. Validate Binary Search Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return check(root, NULL, NULL);
+        
+    }
+    
+    bool check(TreeNode* root, TreeNode* max, TreeNode* min){
+        
+        if(!root)
+            return true;
+        
+        if(min && root->val <= min->val)
+            return false;
+        
+        if(max && root->val >= max->val)
+            return false;
+        
+        return (check(root->left, root, min) && check(root->right, max, root));
+    }
+        
+};
