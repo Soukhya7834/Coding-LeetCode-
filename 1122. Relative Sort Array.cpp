3b3d6eb201a3c3a2adfd134c83b415854a3fd57e
Sam <57521056+sam7834@users.noreply.github.com>
2020-09-04 00:21:09 +0530

    Create 1122. Relative Sort Array.cpp
    https://leetcode.com/problems/relative-sort-array/

---
+++ 1122. Relative Sort Array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        int n = arr1.size();
+        unordered_map<int, int> m;
+        vector<int> res;
+        vector<int> res2;
+
+        for(int i=0; i< n ;i++){
+            m[arr1[i]]++;            //freq of elts
+        }  
+        
+        for(int i=0; i< arr2.size(); i++){
+            int count = m[arr2[i]];     //count = freq
+            
+            for(int j=count; j>0; j--)
+                res.push_back(arr2[i]);
+            m[arr2[i]] = 0;
+        }
+        
+        for(int i=0; i< n ;i++){
+            if(m[arr1[i]] != 0){        //freq of elts
+               int count = m[arr1[i]];
+               for(int j=count; j>0; j--)
+                    res2.push_back(arr1[i]);
+                m[arr1[i]] = 0;
+
+            }                  
+            
+        }  
+        
+        sort(res2.begin(), res2.end());
+        for(int i=0; i< res2.size() ; i++){
+            res.push_back(res2[i]);
+        }
+        
+        return res;
+    }    
+};
