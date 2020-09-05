ed2422e8f12787ad85021b2b99ee14699de9ac0d
Sam <57521056+sam7834@users.noreply.github.com>
2020-09-05 13:56:36 +0530

    Update 189. Rotate Array.cpp
    https://leetcode.com/problems/rotate-array/
    https://leetcode.com/problems/rotate-array/discuss/54277/Summary-of-C++-solutions/205845

--- 189. Rotate Array.cpp
+++ 189. Rotate Array.cpp
@@ -17,3 +17,21 @@ public:
         //return;
     }
 };
+
+BETTER
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %=nums.size();
+        cout << k<< endl;
+        
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        //for(int i = nums.begin(); i < nums.begin()+k; i++)
+            //cout nums[i] << "  ";
+        cout << endl;
+        
+        reverse(nums.begin()+k, nums.end());
+    }
+};
