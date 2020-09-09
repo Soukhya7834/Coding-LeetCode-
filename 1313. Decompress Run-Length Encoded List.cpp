3ef58eeecf440db4cee8e41c406e4d8659378501
Sam <57521056+sam7834@users.noreply.github.com>
2020-09-09 18:00:55 +0530

    Update 1313. Decompress Run-Length Encoded List.cpp
    https://leetcode.com/problems/decompress-run-length-encoded-list/

--- 1313. Decompress Run-Length Encoded List.cpp
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -10,3 +10,29 @@ public:
         return out;
     }
 };
+
+Better 
+
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        
+        vector<int> v;
+        int i,j,k=0, n=nums.size();
+        
+        while(k<n){
+            i=nums[k];
+            j = k+1;
+            cout << "i=" <<i << " ";
+             
+            while(i){
+                v.push_back(nums[j]); i--;
+            }
+                
+            k+=2;  cout << "after "<< "i=" <<i <<endl;
+        }
+        
+        return v;
+    }
+    
+};
