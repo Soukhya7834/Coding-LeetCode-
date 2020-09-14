f07ff8f38a4372836d3551f3669e429938bc085a
Sam <57521056+sam7834@users.noreply.github.com>
2020-09-14 21:12:02 +0530

    Update 1207. Unique Number of Occurrences.cpp
    https://leetcode.com/problems/unique-number-of-occurrences/

--- 1207. Unique Number of Occurrences.cpp
+++ 1207. Unique Number of Occurrences.cpp
@@ -4,17 +4,17 @@ public:
         
         unordered_map<int, int> m;
         unordered_set<int> s;
-
-        int n = A.size()/2;
         
         for(int i=0; i<A.size() ;i++){
             m[A[i]]++;
         }
        
-        map<int, int>::iterator itr; 
+        unordered_map<int, int>::iterator itr; 
+        
         for (itr = m.begin(); itr != m.end(); ++itr) { 
                 s.insert(itr->second); 
-    } 
+        }
+        
         int ms = m.size();
         int ss = s.size();
         
