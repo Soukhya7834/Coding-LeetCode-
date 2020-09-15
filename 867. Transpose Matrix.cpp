32d0a8e36314db2220619572a8e7fc97ab682d73
Sam <57521056+sam7834@users.noreply.github.com>
2020-09-16 00:38:01 +0530

    Create 867. Transpose Matrix.cpp
    https://leetcode.com/problems/transpose-matrix/
    https://leetcode.com/problems/transpose-matrix/discuss/146797/C%2B%2BJavaPython-Easy-Understood

---
+++ 867. Transpose Matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        
+        int M = A.size(), N = A[0].size();
+        vector<vector<int>> B(N, vector<int>(M, 0));
+        
+        //for(colms)  ....for(rows)...smart :)   coz its a transpose
+        for (int j = 0; j < N; j++)
+            for (int i = 0; i < M; i++)
+                B[j][i] = A[i][j];      
+        return B;
+    }
+};
