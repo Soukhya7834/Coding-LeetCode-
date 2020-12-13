d68ca625442f5c330a198c345e17ccb7fb638e7f
Soukhya Mohan <57521056+Soukhya7834@users.noreply.github.com>
2020-12-13 20:18:53 +0530

    Create 1. Two Sum.cpp
    https://leetcode.com/problems/two-sum/

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        map <int, int>::iterator it;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end()) {
+                cout << indices[target - nums[i]] << " " << i;
+                return {indices[target - nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    
+    }
+};
